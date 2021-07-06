e18dfed9078c789d9731deccbe252fc3430cb1c7
Dmitriy <shalberkind@mail.ru>
2021-07-06 16:37:06 +0300

    Using custom brand color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: '#3fbaeb',
+          DEFAULT: '#0fa9e6',
+          dark: '#0c87b8',
+        },
+      },
+    },
   },
   variants: {
     extend: {
-      backgroundColor: ["active"],
+      backgroundColor: ['active'],
     },
   },
   plugins: [],
